9b3a03b0551519b071521ed89e1f03f085d08b0a
alsv06003 <alsv06003@E-PW0234JW>
2023-10-09 14:31:47 +0200

    Add project files.

---
+++ inlämning 3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace inlämning_3._3;
+
+class program
+{
+    static void Main()
+    {
+        int MaxkostnadPerDag = 950;
+        int Timpris = 80;
+
+        Console.Write("Hur många timmar vill du hyra en bil? ");
+        int antalTimmar = int.Parse(Console.ReadLine());
+
+        if (antalTimmar <= MaxkostnadPerDag / Timpris)
+        {
+            int kostnad = antalTimmar * Timpris;
+            Console.WriteLine("Kostnaden för " + antalTimmar + " timmar är " + kostnad + " kr.");
+        }
+        else
+        {
+            Console.WriteLine("Kostnaden för " + antalTimmar + " timmar överstiger den maximala kostnaden per dag " + 950 + " kr. ");
+        }
+    }
+}
